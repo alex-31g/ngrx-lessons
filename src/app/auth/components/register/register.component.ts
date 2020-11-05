e7ac95e41367cbd7c37bd23fae0e4865bd59dab4
alex-31g <al.tsimbalyuk@gmail.com>
2020-11-05 19:10:37 +0200

    add

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+@Component({
+  selector: "nl-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.sass"],
+})
+export class RegisterComponent {}
