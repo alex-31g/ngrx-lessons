797ab524c7b4d8aa5412a9eed626cc3c6535646d
alex-31g <al.tsimbalyuk@gmail.com>
2020-11-06 03:14:43 +0200

    add

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import * as fromAuth from './reducers';
 
 const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: 'register' },
@@ -11,7 +13,16 @@ const routes: Routes = [
 @NgModule({
 	imports: [
 		CommonModule,
-		RouterModule.forChild(routes)
+		RouterModule.forChild(routes),
+
+		/// ngrx has been added to auth-module like feature-module
+		StoreModule.forFeature(
+			// path for the state which we can see in dev tools
+			fromAuth.authFeatureKey, 
+
+			// config reducers for our module
+			fromAuth.reducers
+		)
 	],
 	declarations: [RegisterComponent],
 })
