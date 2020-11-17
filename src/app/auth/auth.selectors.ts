7c742476a63464558dba54156b3ac481767729b2
alex-31g <al.tsimbalyuk@gmail.com>
2020-11-17 16:32:01 +0200

    add

--- src/app/auth/auth.selectors.ts
+++ src/app/auth/auth.selectors.ts
@@ -1,4 +1,7 @@
-import { createSelector } from '@ngrx/store';
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { AuthState } from './reducers';
+
+export const selectAuthState = createFeatureSelector<AuthState>('auth');
 
 // Селектор создается с помощью createSelector().
 // В createSelector() необходимо передать минимум 2 ф-ции-аргумента - mapping и projector:
@@ -6,7 +9,8 @@ import { createSelector } from '@ngrx/store';
 export const isLoggedIn = createSelector(
 	// Первый аргумент - mapping-функция, которая извлекает только необходимый кусок состояния из store (например объект 'auth').
 	// Аргументом принимает глобальный state
-	state => state["auth"],
+	// state => state["auth"],
+	selectAuthState,
 
 	// Второй аргумент - projector-функция - отдает результат работы селектора - true/false, информируя, авторизирован ли юзер или нет.
 	// Аргументом принимает тот кусок состояния, который мы получили с помощью mapping-функции
