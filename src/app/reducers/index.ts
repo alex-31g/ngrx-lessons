ad003ef957c6bbb354960e3ec06b028770ee9f7a
alex-31g <al.tsimbalyuk@gmail.com>
2020-11-25 22:11:14 +0200

    add

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -21,4 +21,24 @@ export const reducers: ActionReducerMap<AppState> = {
 	router: routerReducer
 };
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+// ===============
+// Смотри урок '26. NGRX-4. Metareducers.md'
+
+// В качестве аргумента, metareducer принимает редюсер, который должен быть вызван после metareducer
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+
+	// Metareducer должен возвращать функцию, которая принимает два параметра: состояние и действие
+	return (state, action) => {
+		console.log('state before:', state);
+		console.log('action:', action);
+
+		// Metareducers не должны изменять передаваемые им состояние и действие!!!
+
+		// Сама же функция должна возвращать вызов переданного редюсера
+		return reducer(state, action);
+	}
+}
+
+// Добавляем logger metareducer в массив metaReducers
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [logger] : [];
+// ===============
